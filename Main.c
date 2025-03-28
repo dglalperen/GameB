158be13c394272e23ae38f762e54c21f890f93cd
Alperen Dagli <alperen.dagli43@gmail.com>
2025-03-28 12:02:26 +0100

    added closing feature + mutex for single instance

--- Main.c
+++ Main.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-
 #pragma warning(push, 0)
 #include <windows.h>
 #pragma warning(pop)
-
-LRESULT CALLBACK MainWindowProc(HWND WindowHandle, UINT Message, WPARAM WParam, LPARAM LParam);
-
-DWORD CreateMainGameWindow();
+#include "Main.h"
 
 INT WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLine, INT CmdShow)
 {
@@ -14,6 +10,12 @@ INT WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLine, INT Cm
 	UNREFERENCED_PARAMETER(PrevInstance);
 	UNREFERENCED_PARAMETER(CommandLine);
 	UNREFERENCED_PARAMETER(CmdShow);
+
+	if (GameIsAlreadyRunning() == TRUE) {
+		MessageBoxA(NULL, "Another instance of this program is already running!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+
+		goto Exit;
+	}
 
 	if (CreateMainGameWindow() != ERROR_SUCCESS) {
 		goto Exit;
@@ -29,15 +31,16 @@ INT WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLine, INT Cm
 Exit:
 	return (0);
 }
-
-LRESULT CALLBACK MainWindowProc(
-	HWND WindowHandle,
-	UINT Message,
-	WPARAM WParam,
-	LPARAM LParam)
+LRESULT CALLBACK MainWindowProc(_In_ HWND WindowHandle, _In_ UINT Message, _In_ WPARAM WParam, _In_ LPARAM LParam)
 {
 	switch (Message)
 	{
+		OutputDebugStringA(Message);
+		OutputDebugStringA("\n");
+
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
 	case WM_CREATE:
 		// Initialize the window.
 		return 0;
@@ -61,10 +64,10 @@ LRESULT CALLBACK MainWindowProc(
 	default:
 		return DefWindowProc(WindowHandle, Message, WParam, LParam);
 	}
-	return 0;
+	return (0);
 }
 
-DWORD CreateMainGameWindow() {
+DWORD CreateMainGameWindow(void) {
 	DWORD Result = ERROR_SUCCESS;
 
 	WNDCLASSEXA WindowClass = { 0 };
@@ -75,13 +78,13 @@ DWORD CreateMainGameWindow() {
 	WindowClass.lpfnWndProc = MainWindowProc;
 	WindowClass.cbClsExtra = 0;
 	WindowClass.cbWndExtra = 0;
-	WindowClass.hInstance = GetModuleHandle(NULL); // its the same as passing Instance
+	WindowClass.hInstance = GetModuleHandleA(NULL); // its the same as passing Instance
 	WindowClass.hIcon = LoadIconA(NULL, IDI_APPLICATION);
 	WindowClass.hIconSm = LoadIconA(NULL, IDI_APPLICATION);
 	WindowClass.hCursor = LoadCursorA(NULL, IDC_ARROW);
 	WindowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
 	WindowClass.lpszMenuName = NULL;
-	WindowClass.lpszClassName = "GAME_B_WINDOWCLASS";
+	WindowClass.lpszClassName = GAME_NAME "_WINDOWCLASS";
 
 	if (RegisterClassExA(&WindowClass) == 0)
 	{
@@ -95,9 +98,9 @@ DWORD CreateMainGameWindow() {
 	WindowHandle = CreateWindowExA(
 		WS_EX_CLIENTEDGE,
 		WindowClass.lpszClassName,
-		"Game B",
-		WS_OVERLAPPEDWINDOW,
-		CWMO_DEFAULT, CW_USEDEFAULT, 240, 120, NULL, NULL, GetModuleHandle(NULL), NULL);
+		GAME_NAME,
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		CW_USEDEFAULT, CW_USEDEFAULT, 240, 120, NULL, NULL, GetModuleHandleA(NULL), NULL);
 
 	if (WindowHandle == NULL)
 	{
@@ -110,3 +113,15 @@ Exit:
 
 	return Result;
 }
+
+BOOL GameIsAlreadyRunning(void) {
+	HANDLE Mutex = NULL;
+	Mutex = CreateMutexA(NULL, FALSE, GAME_NAME);
+
+	if (GetLastError() == ERROR_ALREADY_EXISTS) {
+		return(TRUE);
+	}
+	else {
+		return(FALSE);
+	}
+};
---
+++ Main.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define GAME_NAME "Game_B"
+
+LRESULT CALLBACK MainWindowProc(_In_ HWND WindowHandle, _In_ UINT Message, _In_ WPARAM WParam, _In_ LPARAM LParam);
+
+DWORD CreateMainGameWindow(void);
+
+BOOL GameIsAlreadyRunning(void);
